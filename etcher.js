ba36c6d9695082673ce3946dfa0fd037831f2466
Paul Ortiz <paulortiz10@outlook.com>
2023-01-11 11:25:54 -0400

    Made the grid visible

--- etcher.js
+++ etcher.js
@@ -1,11 +1,13 @@
 const container = document.getElementById("container");
 
 function makeGrid (rows, columns) {
-  container.style.setProperty('grid_rows', rows);
-  container.style.setProperty('grid_columns', columns);
+  container.style.setProperty('--grid_rows', rows);
+  container.style.setProperty('--grid_col', columns);
   for (c = 0; c < (rows * columns); c++){
     let cell = document.createElement('div');
     cell.innerText = (c+1);
     container.appendChild(cell).className = 'grid-item';
   };
 };
+
+makeGrid (16,16)
